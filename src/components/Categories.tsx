dea3632a2518fc6e0045081ca047e6d2aa2f94ed
vikuviku6666 <53383503+vikuviku6666@users.noreply.github.com>
2020-11-10 00:57:16 +0100

    category update
    category update

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { ButtonGroup, ButtonToolbar, Button } from "react-bootstrap";
 
-interface ICategoryProps{
+interface ICategoryProps {
   id: number;
   name: string;
 }
@@ -20,25 +20,50 @@ const Categories: React.FC = () => {
     }
     getData();
   }, []);
+ 
   return (
     <>
-      <ButtonToolbar
+        <ButtonToolbar
         className="mt-5 mb-3 justify-content-md-center"
         aria-label="Toolbar with Button groups"
       >
         <ButtonGroup aria-label="First group">
-          <Button href="#" className="mr-3" size="lg" variant="outline-dark">
-            {category[0].name}
+          <Button
+            onClick={handleGetGroup}
+             value={category && category[0].id} 
+            className="mr-3"
+            size="lg"
+            variant="outline-dark"
+          >
+            {category && category[0].name} 
           </Button>
-          <Button href="#" className="mr-3" size="lg" variant="outline-dark">
-            {category[1].name}
+           <Button
+            onClick={handleGetGroup}
+            value={category && category[1].id}
+            className="mr-3"
+            size="lg"
+            variant="outline-dark"
+          >
+            {category && category[1].name}
           </Button>
-          <Button href="#" className="mr-3" size="lg" variant="outline-dark">
-         {category[2].name}
+          <Button
+            onClick={handleGetGroup}
+            value={category && category[2].id}
+            className="mr-3"
+            size="lg"
+            variant="outline-dark"
+          >
+            {category && category[2].name}
           </Button>
-          <Button href="#" className="mr-3" size="lg" variant="outline-dark">
-            {category[3].name}
-          </Button>
+          <Button
+            onClick={handleGetGroup}
+            value={category && category[3].id}
+            className="mr-3"
+            size="lg"
+            variant="outline-dark"
+          >
+            {category && category[3].name}
+          </Button> 
         </ButtonGroup>
       </ButtonToolbar>
     </>
