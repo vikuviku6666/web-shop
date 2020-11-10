8e3470d4a0c13ace792d517e7c3bf7b4f35e41c2
vikuviku6666 <53383503+vikuviku6666@users.noreply.github.com>
2020-11-10 01:24:59 +0100

    fixed small changes
    small changes

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -20,54 +20,46 @@ const Categories: React.FC = () => {
     }
     getData();
   }, []);
- 
+
   return (
     <>
-        <ButtonToolbar
+     <ButtonToolbar
         className="mt-5 mb-3 justify-content-md-center"
         aria-label="Toolbar with Button groups"
       >
         <ButtonGroup aria-label="First group">
           <Button
-            onClick={handleGetGroup}
-             value={category && category[0].id} 
             className="mr-3"
             size="lg"
-            variant="outline-dark"
-          >
+            variant="outline-dark" >
             {category && category[0].name} 
           </Button>
            <Button
-            onClick={handleGetGroup}
-            value={category && category[1].id}
             className="mr-3"
             size="lg"
-            variant="outline-dark"
-          >
+            variant="outline-dark">
             {category && category[1].name}
           </Button>
           <Button
-            onClick={handleGetGroup}
-            value={category && category[2].id}
             className="mr-3"
             size="lg"
-            variant="outline-dark"
-          >
+            variant="outline-dark">
             {category && category[2].name}
           </Button>
           <Button
-            onClick={handleGetGroup}
-            value={category && category[3].id}
-            className="mr-3"
-            size="lg"
-            variant="outline-dark"
-          >
-            {category && category[3].name}
-          </Button> 
-        </ButtonGroup>
-      </ButtonToolbar>
-    </>
-  );
+           className="mr-3"
+           size="lg"
+           variant="outline-dark">
+           {category && category[3].name}
+         </Button> 
+       </ButtonGroup>
+     </ButtonToolbar>
+   </>
+ );
 };
 
 export default Categories;
+           
+           
+            
+           
